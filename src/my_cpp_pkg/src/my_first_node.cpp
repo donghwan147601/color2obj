a196c1191e555ba1b2b53fdbb5cf7211b86a3a0e
Donghwan Lee <donghwan147601@gmail.com>
2026-05-21 20:20:01 +0900

    Update my_first_node.cpp

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -8,7 +8,6 @@ int main(int argc, char ** argv)
 
   printf("hello world my_cpp_pkg package\n");
 
-  // OpenCV가 정상적으로 로드되는지 확인하는 테스트 코드
   cv::Mat test_image = cv::Mat::zeros(480, 640, CV_8UC3);
   printf("OpenCV Matrix created: %d x %d\n", test_image.cols, test_image.rows);
 
